909b9921dc708d8085d4d2b772e1e06cfa20d906
Julian Klitzke <j.klitzke@tu-braunschweig.de>
2024-10-19 13:30:39 +0200

    finished commenting Backend

--- Fianco/src/Backend/Tree.kt
+++ Fianco/src/Backend/Tree.kt
@@ -1,5 +1,12 @@
 package Backend
 
+/**
+ * Represents a node in a tree structure.
+ *
+ * @param T The type of the value stored in the node.
+ * @property value The value stored in the node.
+ * @property children The list of child nodes.
+ */
 data class TreeNode<T>(
     val value: T,
     val children: MutableList<TreeNode<T>> = mutableListOf()
--- Fianco/src/Backend/Zobrist.kt
+++ Fianco/src/Backend/Zobrist.kt
@@ -4,6 +4,11 @@ import java.awt.Point
 import kotlin.random.Random
 import kotlin.random.nextULong
 
+/**
+ * Implements Zobrist hashing for efficient board state hashing.
+ *
+ * Used in the transposition table to uniquely identify board states.
+ */
 class Zobrist {
     private val zobristMap: Array<Array<Array<ULong>>> = Array(9) {
         Array(9) {
@@ -15,20 +20,34 @@ class Zobrist {
 
     var currentBoardHash: ULong = 0u
 
+    /**
+     * Calculates the initial hash value for a given board state.
+     *
+     * @param board The game board.
+     * @return The Zobrist hash value of the board.
+     */
     fun calculateInitialHash(board: Array<Array<Int>>): ULong {
         var zobristHash: ULong = 0u
 
         for (row in 0..8){
             for (column in 0..8){
-                val thirdDim = board[row][column]
-                if (thirdDim != 0){
-                    zobristHash = zobristHash xor zobristMap[row][column][thirdDim-1]
+                val piece = board[row][column]
+                if (piece != 0) {
+                    zobristHash = zobristHash xor zobristMap[row][column][piece - 1]
                 }
             }
         }
         return zobristHash
     }
 
+    /**
+     * Updates the hash value after a move is made.
+     *
+     * @param oldHash The previous hash value.
+     * @param move The move made.
+     * @param playerToMove The player making the move.
+     * @return The new hash value.
+     */
     fun updateHash(oldHash: ULong, move: Pair<Point, Point>, playerToMove: Int): ULong {
         val oldPosition = move.first
         val newPosition = move.second
@@ -39,6 +58,14 @@ class Zobrist {
         return newHash
     }
 
+    /**
+     * Updates the hash value after a capture move.
+     *
+     * @param oldHash The previous hash value.
+     * @param removedPosition The position of the captured piece.
+     * @param playerToMove The player making the move.
+     * @return The new hash value.
+     */
     fun updateHashCapture(oldHash: ULong, removedPosition: Point, playerToMove: Int): ULong {
         val opponentPlayer = if (playerToMove == 1) 2 else 1
         val newHash = oldHash xor zobristMap[removedPosition.x][removedPosition.y][opponentPlayer-1]
